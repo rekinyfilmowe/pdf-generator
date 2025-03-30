dcf937755e81a119b000bc83963d397d491e4e1e
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-03-30 22:29:12 +0200

    Update install-chromium.js

--- install-chromium.js
+++ install-chromium.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
+const browserFetcher = puppeteer.createBrowserFetcher({
+  platform: 'linux',
+});
+
+const revision = '1108766'; // Wersja zgodna z Puppeteer 19.11.1
 
 (async () => {
-    console.log('Installing Chromium for Puppeteer...');
-    await puppeteer.createBrowserFetcher().download(puppeteer.defaultBrowserRevision);
+    console.log(`Installing Chromium revision ${revision}...`);
+    await browserFetcher.download(revision);
     console.log('Chromium installed successfully.');
 })();
