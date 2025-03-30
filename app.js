bbab5e0023f4365c72082eb7e2bdd393b252431b
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-03-30 20:26:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,37 +4,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.post('/', async (req, res) => {
-  const { url } = req.body;
-  console.log("URL to generate PDF:", url);
+    const { url } = req.body;
+    console.log("URL to generate PDF:", url);
 
-  try {
-   const browser = await puppeteer.launch({
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  headless: true
-});
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true
+        });
 
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle0' });
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'networkidle0' });
 
-    const pdf = await page.pdf({ format: 'A4' });
+        const pdfBuffer = await page.pdf({ format: 'A4' });
+        await browser.close();
 
-    await browser.close();
+        // zwróć PDF jako BASE64
+        const pdfBase64 = pdfBuffer.toString('base64');
 
-    res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename=generated.pdf',
-    });
+        res.json({ pdfBase64 });
 
-    res.send(pdf);
-  } catch (error) {
-    console.error('Error generating PDF:', error);
-    res.status(500).send('Error generating PDF');
-  }
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Error generating PDF');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`PDF Generator działa na porcie ${port}`);
+    console.log(`PDF Generator działa na porcie ${port}`);
 });
