66bfbffc277a7ef6c78b72dae68db3995cdb135d
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-03-30 21:15:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/', async (req, res) => {
+app.post('/generate-pdf', async (req, res) => {  // <-- tutaj zmiana na "/generate-pdf"
     const { url } = req.body;
     console.log("URL to generate PDF:", url);
 
