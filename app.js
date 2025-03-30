1ebf2280b9705a57ce03e6022a1ba3f0dff8e359
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-03-31 00:29:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,22 @@ app.post('/', async (req, res) => {
         const page = await browser.newPage();
         await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
 
-        const pdfBuffer = await page.pdf({ format: 'A4' });
+        const pdfBuffer = await page.pdf({
+  format: 'A4',
+  printBackground: true,
+  margin: {
+    top: "30mm",
+    bottom: "30mm",
+    left: "20mm",
+    right: "20mm"
+  },
+  displayHeaderFooter: true,
+  headerTemplate: `<div style="font-size:10px; color: transparent;">.</div>`, // pusty nagłówek
+  footerTemplate: `
+    <div style="width:100%; font-size:10px; text-align:center; color: #999;">
+      Strona <span class="pageNumber"></span> z <span class="totalPages"></span>
+    </div>`
+});
         await browser.close();
 
         const pdfBase64 = pdfBuffer.toString('base64');
