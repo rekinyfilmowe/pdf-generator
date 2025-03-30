344fb397804044b49509f61af90d37fb38c4b65b
rekinyfilmowe <rekinyfilmowe.krzysiek@gmail.com>
2025-03-30 21:40:58 +0200

    Update install-chromium.js

--- install-chromium.js
+++ install-chromium.js
@@ -1,5 +1,8 @@
 const { execSync } = require('child_process');
 
 console.log("Installing Chromium for Puppeteer...");
+
+execSync("npm uninstall puppeteer puppeteer-core", { stdio: 'inherit' });
 execSync("npm install puppeteer@19.11.1 --force", { stdio: 'inherit' });
+
 console.log("Chromium installed successfully.");
